f099b7683b26323d79403e5e047deddbe5c1bbb7
Sergei <75943719+Nightcoder777@users.noreply.github.com>
2020-12-14 10:20:42 +0300

    Little code compression
    Code compressed from 13 to 9 lines

--- xor.cpp
+++ xor.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
+#define decrypt encrypt
 
 std::string encrypt(std::string msg, std::string key){
     std::string tmp(key);
     while(key.size() < msg.size()) key += tmp;
-    for(int i = 0; i < msg.size(); i++){
-        msg[i] ^= key[i];
-    }
-
+    for(int i = 0; i < msg.size(); i++) msg[i] ^= key[i];
     return msg;
 }
-
-std::string decrypt(std::string msg, std::string key) {
-    return encrypt(msg, key);
-} 
