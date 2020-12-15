7925458fdc30ae8c2ca6090c26d90fe4ad234cb4
Sergei <75943719+Nightcoder777@users.noreply.github.com>
2020-12-15 18:45:43 +0300

    Code compression and little fixes
    Code of decryptor compressed from 56 to 46 lines (10 lines)

--- decrypt.cpp
+++ decrypt.cpp
@@ -7,42 +7,33 @@
 #define endl '\n'
 
 int main(int argc, char* argv[]){
-    std::string key;
-    std::string filename;
-    std::string outfile;
-    bool printresult = false;
-    bool binary = false;
+    std::string key, filename, outfile;
+    bool printresult = false, binary = false;
     
     for(int i = 0; i < argc; i++)
     {
-        if((std::string)argv[i] == "-k"){
-            key = argv[i + 1];
-        } else if ((std::string)argv[i] == "-f")
-        {
-            filename = argv[i + 1];
-        } else if ((std::string)argv[i] == "-o")
-        {
-            if((std::string)argv[i + 1] != "@") outfile = argv[i + 1];
+        if((std::string)argv[i] == "-k") key = argv[i + 1];
+        else if ((std::string)argv[i] == "-f") filename = argv[i + 1];
+        else if ((std::string)argv[i] == "-o"){
+            if((std::string)argv[i + 1] != "@") outfile = argv[i + 1]; 
             else printresult = true;
-        } else if ((std::string)argv[i] == "--binary")
-        {
-            binary = true;
-            std::cout << "Opening file in binary mode...\n";
         }
+        else if ((std::string)argv[i] == "--binary") binary = true;       
     }
-    if(!binary) std::cout << "Opening file in normal mode...\n";
+
+    if(!binary) std::cout << "Opening file in normal mode..." << endl;
+    else std::cout << "Opening file in binary mode..." << endl;
     std::string str;
 
     if(!binary){
-        std::ifstream t;
-        t.open(filename);
+        std::ifstream t(filename);
         str = std::string((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
     } else {
         std::ifstream is(filename, std::ios::binary);
         str = std::string((std::istreambuf_iterator<char>(is.rdbuf())), std::istreambuf_iterator<char>());
     }
 
-    std::cout << "Encrypting all data...\n";
+    std::cout << "Decrypting file..." << endl;
     std::string dec = decrypt(str, key);
 
     if(printresult != true){
@@ -50,7 +41,6 @@ int main(int argc, char* argv[]){
         if(!binary) fout.open(outfile); else fout.open(outfile, std::ios::binary);
         fout << dec;
         fout.close();
-        std::cout << "File \"" << filename << "\" decrypted and saved as \"" << outfile << "\"";
+        std::cout << "File \"" << filename << "\" decrypted and saved as \"" << outfile << "\"" << endl;
     } else std::cout << dec;
-    return 0;
 }
